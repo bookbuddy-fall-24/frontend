7462a3b67a46327553084feae2905c909ee01988
Lauren Brubaker <ldelisle@creditacceptance.com>
2024-10-04 11:23:40 -0400

    Books page setup

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,8 +1,27 @@
 /* TODO - add your code to create a functional React component that displays all of the available books in the library's catalog. Fetch the book data from the provided API. Users should be able to click on an individual book to navigate to the SingleBook component and view its details. */
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Books() {
-  return <div>Books</div>;
+  const [bookData, setBookData] = useState([]);
+
+  useEffect(() => {
+    axios(`${import.meta.env.VITE_API_BASE_URL}/api/books`)
+      .then((data) => {
+        console.log(data.data.books);
+        setBookData(data.data.books);
+      })
+
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      {bookData.map((book) => (
+        <p key={book.id}>{book.title}</p>
+      ))}
+    </div>
+  );
 }
 
 export default Books;
