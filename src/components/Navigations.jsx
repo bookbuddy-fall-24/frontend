35590f1740cc37654c6da007dbd918a872388bd9
Lauren Brubaker <ldelisle@creditacceptance.com>
2024-10-04 10:08:54 -0400

    Create basic nav

--- src/components/Navigations.jsx
+++ src/components/Navigations.jsx
@@ -1,8 +1,21 @@
 /* TODO - add your code to create a functional React component that renders a navigation bar for the different views in your single page application. You may consider conditionally rendering some options - for example 'Login' should be available if someone has not logged in yet. */
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navigations() {
-  return <div>Navigations</div>;
+  return (
+    <nav
+      style={{
+        width: "40%",
+        display: "flex",
+        justifyContent: "space-between",
+        margin: "0 auto",
+      }}
+    >
+      <Link to="/">See All Books</Link>
+      <Link to="/login">Login</Link>
+    </nav>
+  );
 }
 
 export default Navigations;
